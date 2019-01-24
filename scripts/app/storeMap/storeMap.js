2cdbf9d77fca86dc985a6d294d391878641e7320
Quintonn <q10athome@gmail.com>
2019-01-24 16:14:28 +0200

    Created repo

---
+++ scripts/app/storeMap/storeMap.js
@@ -0,0 +1,33 @@
+﻿(function (app)
+{
+    'use strict';
+
+    StoreMapController.$inject = ['menuService', '$scope', 'dialogService'];
+
+    function StoreMapController(menuService, $scope, dialogService)
+    {
+        var self = this;
+
+        self.items = [];
+
+        self.$onInit = function ()
+        {
+            
+        }
+    }
+
+    
+    app.component('storeMap', {
+        templateUrl: function ()
+        {
+            return 'scripts/app/storeMap/storeMap.html?v=' + main.version();
+        },
+        controller: StoreMapController,
+        bindings:
+        {
+            parameters: "<",
+            closeDialog: "&",
+        }
+    });
+
+})(angular.module("CALTEX"));
